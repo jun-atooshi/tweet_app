137dfa0a76fb0c5f161db209cc702690b8007ebd
atooshi_jun <ito@atooshi-works.com>
2022-08-04 15:41:17 +0900

    first commit

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,18 @@
+class TweetsController < ApplicationController
+  def index
+  end
+  
+  def new
+    @tweet = Tweet.new
+  end
+  
+  def create
+    Tweet.create(tweet_params)
+    redirect_to tweets_path
+  end
+
+  private
+   def tweet_params
+    params.require(:tweet).permit(:content)
+   end
+ end
