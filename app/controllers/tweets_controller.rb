4679799065b29aae26703e72c366673844c198a8
atooshi_jun <ito@atooshi-works.com>
2022-08-20 16:02:39 +0900

    ファイル変更

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -6,10 +6,30 @@ class TweetsController < ApplicationController
   def new
     @tweet = Tweet.new
   end
-  
+
   def create
-    Tweet.create(tweet_params)
-    redirect_to tweets_path
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to tweets_path, notice: "入力設定が完了しました！"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    @tweet = Tweet.find(params[:id])
+    @tweet.update(tweet_params)
+    redirect_to tweets_path, notice: "更新が完了しました！"
+  end
+ 
+  def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    redirect_to tweets_path, notice: "削除しました！"
   end
 
   private
